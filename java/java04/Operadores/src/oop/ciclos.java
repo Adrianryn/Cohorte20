c8471c209a0dca196344d17aac12d1f0e8d82bdf
adrianryn <adrianryn@gmail.com>
2022-11-24 16:37:41 -0600

    Commit 24

---
+++ java/java04/Operadores/src/oop/ciclos.java
@@ -0,0 +1,53 @@
+package oop;
+
+import java.util.Scanner;
+
+public class ciclos {
+
+	void saludo() {
+		System.out.println("Hola a las funciones en java.");
+	}
+
+	int operacion() {
+		int f = (int) Math.pow(3, 2);
+		return f;
+	}
+
+	static double nuevo() {
+		double h = Math.pow(5, 3);
+		return h;
+	}
+
+	String caracteres() {
+		String nombre = "Hola buen dia";
+		return nombre;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ciclos objC;
+		objC = new ciclos();
+		objC.saludo();
+		
+		System.out.println("Uso de pow " + objC.operacion());
+		
+		System.out.println("Esta es funcion estatica Pow(5,3)"+nuevo());
+		System.out.println(objC.caracteres());
+		int tabla = 2;
+		var scanner = new Scanner(System.in);
+		System.out.println("¿De qué numero quieres la tabla de multiplicar?");
+		tabla = scanner.nextInt();
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(tabla + "*" + i + "=" + tabla * i);
+		}
+		int j = 0;
+		System.out.println("¿De qué numero quieres la otra tabla de multiplicar?");
+		int tablaW = 3;
+		tablaW = scanner.nextInt();
+		while (j <= 10) {
+			System.out.println(tablaW + "*" + j + "=" + (tablaW * j));
+			j++;
+		}
+	}
+
+}
